c9bf239a5aea6e7d08a38cb3aff8db717fff5ecc
pandoragear <andy.dang.it@gmail.com>
2022-02-23 14:21:25 -0600

    Feat: add content to the Main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,15 @@ import React from 'react'
 function Main() {
   return (
     <div>
-        <h1>Main component</h1> 
+        <h1>Facts about React</h1> 
+        <ul>
+            <li> React is not a framework </li>
+            <li> virtual DOM is faster than real DOM</li>
+            <li> Has a strong community </li>
+            <li> Was first released in 2013</li>
+            <li> Has well over 100k stars on GitHub</li>
+            <li> React development and SEO go together</li>
+            </ul> 
     </div>
   )
 }
