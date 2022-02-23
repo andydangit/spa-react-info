eecb3bff5fc2d6b9a36f1f306dbac00432a7a585
pandoragear <andy.dang.it@gmail.com>
2022-02-23 10:41:53 -0600

    Feat: add style to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
+import logo from "../image/react.png";
+
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div>
-      <h1> Navbar component</h1>
-    </div>
+    <nav className="navbar">
+      <img src={logo} alt="React Logo" width="50px" />
+      <h2> React Facts </h2>
+    </nav>
   );
 }
 
