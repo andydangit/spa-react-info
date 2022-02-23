a84db0cc146f3d901921ff5f88e2f28deb2bb360
pandoragear <andy.dang.it@gmail.com>
2022-02-23 14:44:41 -0600

    Feat: Add styles to Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./Main.css";
 
 function Main() {
   return (
-    <div>
-        <h1>Facts about React</h1> 
-        <ul>
-            <li> React is not a framework </li>
-            <li> virtual DOM is faster than real DOM</li>
-            <li> Has a strong community </li>
-            <li> Was first released in 2013</li>
-            <li> Has well over 100k stars on GitHub</li>
-            <li> React development and SEO go together</li>
-            </ul> 
-    </div>
-  )
+    <main>
+      <h1 className="mainHeading">Facts about React</h1>
+      <ul className="reactFacts">
+        <li> React is not a framework </li>
+        <li> Has a strong community </li>
+        <li> Was first released in 2013</li>
+        <li> Has well over 100k stars on GitHub</li>
+        <li> virtual DOM is faster than real DOM</li>
+        <li> React development and SEO go together</li>
+      </ul>
+    </main>
+  );
 }
 
-export default Main
+export default Main;
