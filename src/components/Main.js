abfd8bd8d9b876caec8d06a3f33d6bab52279e59
pandoragear <andy.dang.it@gmail.com>
2022-02-24 11:36:22 -0600

    Refactor: add new facts to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,9 +8,11 @@ function Main() {
       <ul className="reactFacts">
         <li> React is not a framework </li>
         <li> Has a strong community </li>
-        <li> Was first released in 2013</li>
+        <li> Was first released in 2013</li>        
         <li> Has well over 100k stars on GitHub</li>
         <li> virtual DOM is faster than real DOM</li>
+        <li> Can update the DOM asynchronously</li>
+        <li> Minified files size without dependencies</li>
         <li> React development and SEO go together</li>
       </ul>
     </main>
